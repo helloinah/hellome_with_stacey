29db9a79ce2256e0b4c402c1bf9ffac76f3c6833
Helloinah <hello.inah@gmail.com>
2011-06-24 23:07:28 +1000

    final files from version 1

--- public/docs/js/navigation.js
+++ public/docs/js/navigation.js
@@ -7,27 +7,14 @@ $(document).ready(function(){
 	var s_button = $('#sort_button');
 	
 	 $('#text_button').change(function () {
-		 t_box.css('visibility', 'visible');
-		 i_box.css('visibility', 'hidden');
-		 s_button.css('visibility', 'hidden');
-		 t = true;
-		 i = false;
+		   window.location = "./";
+
         });
 	$('#image_button').change(function () {
-		  t_box.css('visibility', 'hidden');
-		 i_box.css('visibility', 'visible');
-		  s_button.css('visibility', 'visible');
-			i = true;
-			t = false;
+		    window.location = "./?/Image_index.html";
+
         });
-	$('#image_button_a').click(function(){
-			$('#image_button').trigger('change');
-		});
-	$('#text_button_a').click(function(){
-		$(this).attr("href", "./");
-	  	$('#text_button').trigger('change');
-	 
-		});
+	
 	
 
 });
